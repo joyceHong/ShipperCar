64aab8668712bae51484834c3e01c555471fab59
joyceHong <joyceHong0827@gmail.com>
2016-10-25 10:18:21 +0800

    第四個紅燈

--- ShiiperCarTest/UnitTest1.cs
+++ ShiiperCarTest/UnitTest1.cs
@@ -24,7 +24,7 @@ namespace ShiiperCarTest
         [TestMethod]
         public void 第一集買了一本而且第二集也買了一本期望為190()
         {
-            //期望第二本有0.9折扣，但不同版本
+            //期望第二本有0.95折扣，但不同版本
             List<Book> books = new List<Book>() {
                 new Book() {  Name="哈利波特",Version="1", Quantity=1 },
                 new Book() {  Name="哈利波特",Version="2", Quantity=1 }
@@ -39,7 +39,7 @@ namespace ShiiperCarTest
         [TestMethod]
         public void 一二三集各買了一本期望為270()
         {
-            //期望第二本有0.9折扣，但不同版本
+            //期望第三本有0.9折扣，但不同版本
             List<Book> books = new List<Book>() {
                 new Book() {  Name="哈利波特",Version="1", Quantity=1 },
                 new Book() {  Name="哈利波特",Version="2", Quantity=1 },
@@ -48,6 +48,24 @@ namespace ShiiperCarTest
 
             OrderShiiperCar shipperCarOb = new OrderShiiperCar();
             var expected = 270;
+            var actual = shipperCarOb.TotalValue(books);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void 一二三四集各買了一本期望為320()
+        {
+            //期望第二本有0.8折扣，但不同版本
+            List<Book> books = new List<Book>() {
+                new Book() {  Name="哈利波特",Version="1", Quantity=1 },
+                new Book() {  Name="哈利波特",Version="2", Quantity=1 },
+                new Book() {  Name="哈利波特",Version="3", Quantity=1 },
+                new Book() {  Name="哈利波特",Version="4", Quantity=1 }
+            };
+
+            OrderShiiperCar shipperCarOb = new OrderShiiperCar();
+            var expected = 320;
             var actual = shipperCarOb.TotalValue(books);
             Assert.AreEqual(expected, actual);
         }
