eac86db0dc958c1ac92bd3ccab5a4bb117b4b8a4
joyceHong <joyceHong0827@gmail.com>
2016-10-25 10:12:44 +0800

    第三個紅燈

--- ShiiperCarTest/UnitTest1.cs
+++ ShiiperCarTest/UnitTest1.cs
@@ -35,5 +35,21 @@ namespace ShiiperCarTest
             var actual = shipperCarOb.TotalValue(books);
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void 一二三集各買了一本期望為270()
+        {
+            //期望第二本有0.9折扣，但不同版本
+            List<Book> books = new List<Book>() {
+                new Book() {  Name="哈利波特",Version="1", Quantity=1 },
+                new Book() {  Name="哈利波特",Version="2", Quantity=1 },
+                new Book() {  Name="哈利波特",Version="3", Quantity=1 }
+            };
+
+            OrderShiiperCar shipperCarOb = new OrderShiiperCar();
+            var expected = 270;
+            var actual = shipperCarOb.TotalValue(books);
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
